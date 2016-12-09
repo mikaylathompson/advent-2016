78bd17e57bde52b28004c8204a988a2643a74a46
Mikayla Thompson <mthompson@butterflynetinc.com>
2016-12-08 22:27:32 -0500

    Complete day_4 part 2

--- day_4.py
+++ day_4.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import string
 
 def generate_checksum(c):
     # ordered = sorted(commonality_list, key=lambda x: )
@@ -18,7 +19,29 @@ def verify_checksum(line):
         return 0
 
 
+def shift(plaintext, shift, alphabet=string.ascii_lowercase):
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    cipher = plaintext.maketrans(alphabet, shifted_alphabet)
+    return plaintext.translate(cipher)
+
+
+def decode(line):
+    if not verify_checksum(line):
+        return
+    secret_id = verify_checksum(line)
+    letters = ''.join(line[:-7].split('-')[:-1])
+    shift_val = secret_id % 26
+    translated = shift(letters, shift_val)
+    if 'north' in translated:
+        print(secret_id, translated)
+
+
 
 if __name__ == "__main__":
+    # Part 1
+    # with open('day_4_input.txt', 'r') as f:
+    #     print(sum([verify_checksum(line.strip()) for line in f]))
+    # Part 2
     with open('day_4_input.txt', 'r') as f:
-        print(sum([verify_checksum(line.strip()) for line in f]))
+        for line in f:
+            decode(line.strip())
