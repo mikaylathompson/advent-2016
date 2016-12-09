45d9310f8d34b217576f382d0f0327d5a46c1255
Mikayla Thompson <mthompson@butterflynetinc.com>
2016-12-08 23:43:33 -0500

    Add Part 2 for day_3

--- day_3.py
+++ day_3.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 def is_valid(a, b, c):
     return a + b > c
@@ -8,6 +9,23 @@ def is_valid_line(line):
     return is_valid(*sorted(vals))
 
 
+def count_valid_trianges(l1, l2, l3):
+    vals = list(map(list, zip(*[[int(x) for x in line.strip().split()] for line in [l1, l2, l3]])))
+    return sum([1 for row in vals if is_valid(*sorted(row))])
+
+
 with open("day_3_input.txt", 'r') as f:
-    truth = [1 for line in f if is_valid_line(line)]
-    print(sum(truth))
+    # Part 1
+    # truth = [1 for line in f if is_valid_line(line)]
+    # print(sum(truth))
+
+    # Part 2
+    n_valid_tris = 0
+    while True:
+        line1 = f.readline()
+        line2 = f.readline()
+        line3 = f.readline()
+        if not line3:
+            print(n_valid_tris)
+            break
+        n_valid_tris += count_valid_trianges(line1, line2, line3)
